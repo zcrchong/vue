5779c407c925b7c3e9feb665f3bd830f367a05b8
chaoranzuo <15269876632@163.com>
2023-12-04 12:15:02 +0800

    docs: eventsMixin done

--- src/core/instance/events.ts
+++ src/core/instance/events.ts
@@ -55,9 +55,14 @@ export function updateComponentListeners(
   )
   target = undefined
 }
-
+// 在使用Vue做开发的时候，我们一定经常使用到$emit、$on、$off和$once等几个实例方法，eventsMixin主要做的就是在Vue.prototype上定义这四个实例方法
+// eventsMixin()所做的事情就是使用发布-订阅模式来处理事件
 export function eventsMixin(Vue: typeof Component) {
   const hookRE = /^hook:/
+  /**
+   * 我们发现在Vue源码中，$on方法还接受一个数组event，这其实是在Vue2.2.0版本以后才有的，当传递一个event数组时，会通过遍历数组的形式递归调用$on方法。
+   * 我们还发现，所有$on的事件全部绑定在_events私有属性上，这个属性其实是在我们上面已经提到过的initEvents()方法中被定义的。
+   */
   Vue.prototype.$on = function (
     event: string | Array<string>,
     fn: Function
@@ -68,7 +73,7 @@ export function eventsMixin(Vue: typeof Component) {
         vm.$on(event[i], fn)
       }
     } else {
-      ;(vm._events[event] || (vm._events[event] = [])).push(fn)
+      ; (vm._events[event] || (vm._events[event] = [])).push(fn)
       // optimize hook:event cost by using a boolean flag marked at registration
       // instead of a hash lookup
       if (hookRE.test(event)) {
@@ -77,7 +82,7 @@ export function eventsMixin(Vue: typeof Component) {
     }
     return vm
   }
-
+  // 在回调之后立马调用$off
   Vue.prototype.$once = function (event: string, fn: Function): Component {
     const vm: Component = this
     function on() {
@@ -93,6 +98,12 @@ export function eventsMixin(Vue: typeof Component) {
     event?: string | Array<string>,
     fn?: Function
   ): Component {
+    /**
+     * $off方法的实现，相对来说比较复杂一点，因为它需要根据不同的传参做不同的事情：
+      1. 当没有提供任何参数时，移除全部事件监听。
+      2. 当只提供event参数时，只移除此event对应的监听器。
+      3. 同时提供event参数和fn回调，则只移除此event对应的fn这个监听器。
+     */
     const vm: Component = this
     // all
     if (!arguments.length) {
@@ -135,23 +146,28 @@ export function eventsMixin(Vue: typeof Component) {
       if (lowerCaseEvent !== event && vm._events[lowerCaseEvent]) {
         tip(
           `Event "${lowerCaseEvent}" is emitted in component ` +
-            `${formatComponentName(
-              vm
-            )} but the handler is registered for "${event}". ` +
-            `Note that HTML attributes are case-insensitive and you cannot use ` +
-            `v-on to listen to camelCase events when using in-DOM templates. ` +
-            `You should probably use "${hyphenate(
-              event
-            )}" instead of "${event}".`
+          `${formatComponentName(
+            vm
+          )} but the handler is registered for "${event}". ` +
+          `Note that HTML attributes are case-insensitive and you cannot use ` +
+          `v-on to listen to camelCase events when using in-DOM templates. ` +
+          `You should probably use "${hyphenate(
+            event
+          )}" instead of "${event}".`
         )
       }
     }
+    /**
+     * 从整体上看，$emit实现方法非常简单，第一步从_events对象中取出对应的cbs，接着一个个遍历cbs数组、调用并传参。
+
+     */
     let cbs = vm._events[event]
     if (cbs) {
       cbs = cbs.length > 1 ? toArray(cbs) : cbs
       const args = toArray(arguments, 1)
       const info = `event handler for "${event}"`
       for (let i = 0, l = cbs.length; i < l; i++) {
+        // invokeWithErrorHandling代码中会使用try/catch把我们函数调用并执行的地方包裹起来，当函数调用出错时，会执行Vue的handleError()方法，这种做法不仅更加友好，而且对错误处理也非常有用。
         invokeWithErrorHandling(cbs[i], vm, args, vm, info)
       }
     }
